d485b8947ce1279ff3f7c6febfb641aedfb852be
unknown <mrtukaramshinde1803@gmail.com>
2025-09-25 15:04:18 +0530

    update c file

---
+++ programming.copp4.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+	char ch;
+	printf("Enter any character");
+	//Reads a character 
+	ch=getchar();
+	//Display that character 
+	putchar(ch);
+    return 0;	
+}
